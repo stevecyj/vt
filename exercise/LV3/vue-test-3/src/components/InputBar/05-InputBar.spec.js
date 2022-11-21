2d7408f6413e5d88abc120208f4c32d4028e8469
Steve <ycsteven.tsao@gmail.com>
2022-11-21 20:27:20 +0800

    set props

--- exercise/LV3/vue-test-3/src/components/InputBar/05-InputBar.spec.js
+++ exercise/LV3/vue-test-3/src/components/InputBar/05-InputBar.spec.js
@@ -1,4 +1,20 @@
-import { mount } from "@vue/test-utils";
-import InputBar from "@/components/InputBar";
+import { mount } from '@vue/test-utils';
+import InputBar from '@/components/InputBar';
 
-describe("InputBar.vue", () => {});
+describe('InputBar.vue', () => {
+  it('should test InputBar', () => {
+    const wrapper = mount(InputBar);
+
+    wrapper.find('input.name').setValue('Mike');
+    wrapper.find('input.email').setValue('1234@gmail.com');
+    // wrapper.find('input.name').setValue('Mike');
+    expect(wrapper.find('input.name').element.value).toBe('Mike');
+    expect(wrapper.find('input.email').element.value).toBe('1234@gmail.com');
+  });
+
+  it('test thousand', async () => {
+    const wrapper = mount(InputBar);
+    await wrapper.setProps({ thousand: '1345000' });
+    expect(wrapper.find('input.num').element.value).toBe('1,345,000');
+  });
+});
