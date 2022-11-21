2711fa6a3e07ad2defa93774c074eebf18f03a18
Steve <ycsteven.tsao@gmail.com>
2022-11-21 20:00:34 +0800

    set props

--- exercise/LV3/vue-test-3/src/components/ContentBox/04-ContentBox.spec.js
+++ exercise/LV3/vue-test-3/src/components/ContentBox/04-ContentBox.spec.js
@@ -1,4 +1,10 @@
-import { mount } from "@vue/test-utils";
-import ContentBox from "@/components/ContentBox";
+import { mount } from '@vue/test-utils';
+import ContentBox from '@/components/ContentBox';
 
-describe("ContentBox.vue", () => {});
+describe('ContentBox.vue', () => {
+  it('should be props', async () => {
+    const wrapper = mount(ContentBox);
+    await wrapper.setProps({ isOpen: false });
+    expect(wrapper.find('.box').isVisible()).toBe(false);
+  });
+});
