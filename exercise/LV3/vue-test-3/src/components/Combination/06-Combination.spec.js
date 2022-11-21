cddb40cfc789c44c530d75915faacc1ddd57e229
Steve <ycsteven.tsao@gmail.com>
2022-11-21 20:52:52 +0800

    emit

--- exercise/LV3/vue-test-3/src/components/Combination/06-Combination.spec.js
+++ exercise/LV3/vue-test-3/src/components/Combination/06-Combination.spec.js
@@ -1,4 +1,13 @@
-import { mount } from "@vue/test-utils";
-import Combination from "@/components/Combination";
+import { mount } from '@vue/test-utils';
+import Combination from '@/components/Combination';
 
-describe("Combination.vue", () => {});
+describe('Combination.vue', () => {
+  it('test emit', async () => {
+    const wrapper = mount(Combination);
+    await wrapper.setProps({ thousand: 1000000 });
+    await wrapper.find('button').trigger('click');
+
+    // console.log(wrapper.emitted('Combination'));
+    expect(wrapper.emitted('Combination')[0][0]).toBe('1,000,000');
+  });
+});
