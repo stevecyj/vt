39f08fe2f114c52bb051e9e87c6e6bf47c26a243
stevecyj <ycsteven.tsao@gmail.com>
2022-11-22 15:53:38 +0800

    spyOn, test fetch api data

--- exercise/LV5/vue-test-5/src/App.spec.js
+++ exercise/LV5/vue-test-5/src/App.spec.js
@@ -1,30 +1,32 @@
-import { mount } from "@vue/test-utils";
-import App from "@/App.vue";
+import { mount } from '@vue/test-utils';
+import App from '@/App.vue';
+import { gitHubRequest } from '@/api';
+
 const mockData = {
-  login: "MikeCheng1208",
+  login: 'MikeCheng1208',
   id: 7043304,
-  node_id: "MDQ6VXNlcjcwNDMzMDQ=",
-  avatar_url: "https://avatars.githubusercontent.com/u/7043304?v=4",
-  gravatar_id: "",
-  url: "https://api.github.com/users/MikeCheng1208",
-  html_url: "https://github.com/MikeCheng1208",
-  followers_url: "https://api.github.com/users/MikeCheng1208/followers",
+  node_id: 'MDQ6VXNlcjcwNDMzMDQ=',
+  avatar_url: 'https://avatars.githubusercontent.com/u/7043304?v=4',
+  gravatar_id: '',
+  url: 'https://api.github.com/users/MikeCheng1208',
+  html_url: 'https://github.com/MikeCheng1208',
+  followers_url: 'https://api.github.com/users/MikeCheng1208/followers',
   following_url:
-    "https://api.github.com/users/MikeCheng1208/following{/other_user}",
-  gists_url: "https://api.github.com/users/MikeCheng1208/gists{/gist_id}",
+    'https://api.github.com/users/MikeCheng1208/following{/other_user}',
+  gists_url: 'https://api.github.com/users/MikeCheng1208/gists{/gist_id}',
   starred_url:
-    "https://api.github.com/users/MikeCheng1208/starred{/owner}{/repo}",
-  subscriptions_url: "https://api.github.com/users/MikeCheng1208/subscriptions",
-  organizations_url: "https://api.github.com/users/MikeCheng1208/orgs",
-  repos_url: "https://api.github.com/users/MikeCheng1208/repos",
-  events_url: "https://api.github.com/users/MikeCheng1208/events{/privacy}",
+    'https://api.github.com/users/MikeCheng1208/starred{/owner}{/repo}',
+  subscriptions_url: 'https://api.github.com/users/MikeCheng1208/subscriptions',
+  organizations_url: 'https://api.github.com/users/MikeCheng1208/orgs',
+  repos_url: 'https://api.github.com/users/MikeCheng1208/repos',
+  events_url: 'https://api.github.com/users/MikeCheng1208/events{/privacy}',
   received_events_url:
-    "https://api.github.com/users/MikeCheng1208/received_events",
-  type: "User",
+    'https://api.github.com/users/MikeCheng1208/received_events',
+  type: 'User',
   site_admin: false,
   name: null,
   company: null,
-  blog: "https://www.youtube.com/channel/UC7ArpUezGLX-dZ0FTS_jVMQ",
+  blog: 'https://www.youtube.com/channel/UC7ArpUezGLX-dZ0FTS_jVMQ',
   location: null,
   email: null,
   hireable: null,
@@ -34,8 +36,21 @@ const mockData = {
   public_gists: 28,
   followers: 221,
   following: 37,
-  created_at: "2014-03-24T04:21:10Z",
-  updated_at: "2022-02-10T00:16:31Z",
+  created_at: '2014-03-24T04:21:10Z',
+  updated_at: '2022-02-10T00:16:31Z',
 };
 
-describe("App.vue", () => {});
+describe('App.vue', () => {
+  it('test fetch api data', async () => {
+    const wrapper = mount(App);
+
+    jest.spyOn(gitHubRequest, 'get');
+
+    gitHubRequest.get.mockImplementation(() => {
+      return Promise.resolve({ data: mockData });
+    });
+
+    await wrapper.find('button').trigger('click');
+    expect(JSON.parse(wrapper.find('pre').text())).toEqual(mockData);
+  });
+});
