370d4570592a17ce5f814455c24871688fd0f4db
Steve <ycsteven.tsao@gmail.com>
2022-11-23 07:15:20 +0800

    test:set different store default value

--- exercise/LV6/vue-test-6/src/store/store.spec.js
+++ exercise/LV6/vue-test-6/src/store/store.spec.js
@@ -8,4 +8,39 @@ describe('test Store commit', () => {
     expect($store.state.isOpen).toBe(true);
     expect($store.getters['isOpenAddStr']).toBe('狀態是 true');
   });
+
+  const CreateStoreDefault = (initState) => {
+    return createStore({
+      state() {
+        return {
+          count: 0,
+          ...initState,
+        };
+      },
+      actions: {
+        addCount(context) {
+          context.commit('addCount');
+        },
+      },
+      mutations: {
+        addCount(state) {
+          state.count++;
+        },
+      },
+    });
+  };
+
+  it('test default store state 1', () => {
+    const $store = CreateStoreDefault({ count: 5 });
+    console.log('1=>', $store.state.count);
+    $store.dispatch('addCount');
+    expect($store.state.count).toBe(6);
+  });
+
+  it('test default store state 2', () => {
+    const $store = CreateStoreDefault({ count: 8 });
+    console.log('2=>', $store.state.count);
+    $store.dispatch('addCount');
+    expect($store.state.count).toBe(9);
+  });
 });
