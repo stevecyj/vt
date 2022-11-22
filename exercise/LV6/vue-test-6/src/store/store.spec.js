d4bed8fa31f5ae9acfd69049d0c6a8383d2fe27f
Steve <ycsteven.tsao@gmail.com>
2022-11-22 23:28:58 +0800

    directly test store

--- exercise/LV6/vue-test-6/src/store/store.spec.js
+++ exercise/LV6/vue-test-6/src/store/store.spec.js
@@ -1,1 +1,10 @@
-describe("test Store commit", () => {});
+import { createStore } from 'vuex';
+import { store } from '@/store';
+
+describe('test Store commit', () => {
+  it('test store commit', () => {
+    const $store = createStore(store);
+    $store.dispatch('handleOpenState', true);
+    expect($store.state.isOpen).toBe(true);
+  });
+});
