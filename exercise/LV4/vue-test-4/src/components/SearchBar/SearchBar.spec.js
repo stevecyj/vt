7066eed60c83c9057a2ce6760a061cd0a907abab
Steve <ycsteven.tsao@gmail.com>
2022-11-22 08:19:23 +0800

    assertion:toBe

--- exercise/LV4/vue-test-4/src/components/SearchBar/SearchBar.spec.js
+++ exercise/LV4/vue-test-4/src/components/SearchBar/SearchBar.spec.js
@@ -1,4 +1,23 @@
-import { mount } from "@vue/test-utils";
-import SearchBar from "@/components/SearchBar";
+import { mount } from '@vue/test-utils';
+import SearchBar from '@/components/SearchBar';
 
-describe("SearchBar.vue", () => {});
+describe('SearchBar.vue', () => {
+  it('should SearchBar Title', async () => {
+    const wrapper = mount(SearchBar);
+
+    expect(wrapper.find('.title').text()).toBe(
+      'Vue 單元測試 vue-test-utils｜入門'
+    );
+
+    await wrapper.find('.text3').trigger('click');
+
+    const titleArr = [
+      '館長成吉思汗- YouTube',
+      '陳之漢- 维基百科，自由的百科全書',
+    ];
+
+    wrapper.findAll('.title').forEach((item, idx) => {
+      expect(item.text()).toBe(titleArr[idx]);
+    });
+  });
+});
