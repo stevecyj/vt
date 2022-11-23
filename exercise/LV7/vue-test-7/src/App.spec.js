1a2e72ead038cd9d470b42c1ca602f53ce8b55e6
Steve <ycsteven.tsao@gmail.com>
2022-11-23 08:12:42 +0800

    stubs mount router-link & router-view

--- exercise/LV7/vue-test-7/src/App.spec.js
+++ exercise/LV7/vue-test-7/src/App.spec.js
@@ -1,8 +1,12 @@
-import { mount } from "@vue/test-utils";
-import App from "./App.vue";
+import { mount } from '@vue/test-utils';
+import App from './App.vue';
 
-describe("App.vue", () => {
-  it("route test", () => {
-    const wrapper = mount(App);
+describe('App.vue', () => {
+  it('route test', () => {
+    const wrapper = mount(App, {
+      global: {
+        stubs: ['router-link', 'router-view'],
+      },
+    });
   });
 });
