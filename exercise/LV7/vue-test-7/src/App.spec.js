e32c97ad7d66b9a8e47612eaf628c538012626ee
stevecyj <ycsteven.tsao@gmail.com>
2022-11-23 12:57:21 +0800

    useRouter mockImplementation access each route

--- exercise/LV7/vue-test-7/src/App.spec.js
+++ exercise/LV7/vue-test-7/src/App.spec.js
@@ -1,20 +1,42 @@
 import { mount } from '@vue/test-utils';
 import App from './App.vue';
+import { useRoute } from 'vue-router';
 
 jest.mock('vue-router', () => ({
-  //
   useRoute: jest.fn(() => ({ path: '' })),
 }));
 
 describe('App.vue', () => {
-  it('route test', () => {
+  it('route test 1', () => {
+    useRoute.mockImplementation(() => ({ path: '' }));
     const wrapper = mount(App, {
       global: {
         stubs: ['router-link', 'router-view'],
       },
     });
 
-    // console.log(wrapper.html());
     expect(wrapper.find('#home').classes('active')).toBe(true);
+  });
+
+  it('route test 2', () => {
+    useRoute.mockImplementation(() => ({ path: '/about' }));
+    const wrapper = mount(App, {
+      global: {
+        stubs: ['router-link', 'router-view'],
+      },
+    });
+
+    expect(wrapper.find('#about').classes('active')).toBe(true);
+  });
+
+  it('route test 3', () => {
+    useRoute.mockImplementation(() => ({ path: '/courses' }));
+    const wrapper = mount(App, {
+      global: {
+        stubs: ['router-link', 'router-view'],
+      },
+    });
+
+    expect(wrapper.find('#courses').classes('active')).toBe(true);
   });
 });
