94f0b9f42cceb5a17cfd6d1bbc34ca389bd3a6bb
Steve <ycsteven.tsao@gmail.com>
2022-11-24 08:25:56 +0800

    slot

--- exercise/LV8/vue-test-8/src/components/TitleBar.spec.js
+++ exercise/LV8/vue-test-8/src/components/TitleBar.spec.js
@@ -1,8 +1,23 @@
-import { mount } from "@vue/test-utils";
-import TitleBar from "./TitleBar.vue";
+import { mount } from '@vue/test-utils';
+import TitleBar from './TitleBar.vue';
 
-describe("TitleBar.vue", () => {
-  it("renders slots content", () => {
-    const wrapper = mount(TitleBar);
+describe('TitleBar.vue', () => {
+  it('renders slots content', () => {
+    const wrapper = mount(TitleBar, {
+      slots: {
+        default: '今天天氣很好',
+        detail: '這是複製的',
+      },
+    });
+
+    console.log(wrapper.html());
+
+    expect(wrapper.find('.title').html()).toBe(
+      '<h1 class="title">今天天氣很好</h1>'
+    );
+
+    expect(wrapper.find('.detail').html()).toBe(
+      '<p class="detail">這是複製的</p>'
+    );
   });
 });
